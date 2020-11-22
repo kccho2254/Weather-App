210e5f44e5581739f90b2eafc7a7c2d25426ab05
Kevin Cho <kccho2254@gmail.com>
2020-11-21 22:54:39 -0600

    Added more Javascript to create list items for each ajax call

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 //html doesn't need to be extensive
 
 $(document).ready(function () {
-
     //var queryURL
     //  holds the API key
     //  console log response.name, response.main.temp/humidity/speed
@@ -24,7 +23,8 @@ $(document).ready(function () {
 
     function displayWeatherInfo(cityName) {
 
-        var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0a9a4df09118ecfa80161ac660502a6e&units=imperial`;
+
+        var queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0a9a4df09118ecfa80161ac660502a6e&units=imperial`
 
         $.ajax({
             url: queryURL,
@@ -32,26 +32,83 @@ $(document).ready(function () {
         }).then(function (res) {
             console.log(res);
 
-            // var card = $('div').addClass('card');
-            // // var cardBody = $('div').addClass('card-body');
-            // // var cityTitle = $('<h3>').text(res.name);
-            // // var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+            // var cardBody = $('div').addClass('card-body');
+            // var img = $("<img>").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
 
-            var feelsLike = $("<td>").text("Feels like: " + res.main.feels_like + "°F");
-            var humidity = $("<td>").text("Humidity is: " + res.main.humidity);
-            var temp = $("<td>").text("Temperature is: " + res.main.temp + "°F");
+            var feelsLike = $("<ul>").text("Feels like: " + res.main.feels_like + "°F");
+            var humidity = $("<ul>").text("Humidity is: " + res.main.humidity + "%");
+            var temp = $("<ul>").text("Temperature is: " + res.main.temp + "°F");
 
-            temp.append(card)
-            humidity.append(card);
-            feelsLike.append(card);
-            $("#todayWeather").append(cityName, lSection, mSection, nSection);
+            $("#todayWeather").append(cityName, feelsLike, humidity, temp);
 
-        });
+
+            var uvQueryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${res.coord.lat}&lon=${res.coord.lon}&appid=0a9a4df09118ecfa80161ac660502a6e`;
+
+            console.log(res.coord.lat);
+            console.log(res.coord.lon);
+
+            $.ajax({
+                url: uvQueryURL,
+                method: "GET"
+
+            }).then(function (res) {
+
+                if (res.value > 7) {
+                    var uvIndex = $("<ul>").text("UV index is: " + res.value).addClass('text-danger');
+                    $("#todayWeather").append(uvIndex);
+                }
+                else if (res.value > 4 && res.value < 7) {
+                    var uvIndex = $("<ul>").text("UV index is: " + res.value).addClass('text-warning');
+                    $("#todayWeather").append(uvIndex);
+                }
+                if (res.value > 0 && res.value < 4) {
+                    var uvIndex = $("<ul>").text("UV index is: " + res.value).addClass('text-success');
+                    $("#todayWeather").append(uvIndex);
+                }
+
+
+            })
+
+            var fiveDayQueryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=0a9a4df09118ecfa80161ac660502a6e`;
+            // date, temp, humidity, icon
+
+            $.ajax({
+                url: fiveDayQueryURL,
+                method: "GET"
+
+            }).then(function (res) {
+                console.log(res.list[0]);
+                for (i = 0; i < 5; i++) {
+                   
+                    // var imageLocation = image.attr('src', '${res.list[i+0].weather[0].icon}.png');
+                    
+                    // var image = res.list[i+0].weather.icon;
+                    // image.attr("src", `https://openweathermap.org/img/wn/${image}@2x.png`);
+                    
+                    // $("<img>").append(image);
+
+
+                    console.log(res.list[i+0].dt_txt);
+                    console.log(res.list[i+0].main.temp);
+                    console.log(res.list[i+0].main.humidity);
+                
+
+                    var fiveDay = $("<ul>").text(res.list[i+0].dt_txt + " Temperature: " + res.list[i+0].main.temp +"°F + Humidity: "+ res.list[i+0].main.humidity + "%");
+                  
+                    $('#fiveDay').append(fiveDay);
+
+
+
+                }
+            })
+
+        })
     }
     $('#search-button').on("click", function () {
+
+        $("#todayWeather, #fiveDay").empty();
         var city = $('#search-value').val();
         displayWeatherInfo(city);
 
     })
-
 })
